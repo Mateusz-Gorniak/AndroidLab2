a23f411b6d15b67d378f1354d4d0c0e06ea6bcd4
Mateusz <mateusz.gorniak@student.wat.edu.pl>
2021-04-24 19:43:14 +0200

    2.1

---
+++ app/src/main/java/pl/gorniak/bookweblab2/AboutProgramActivity.java
@@ -0,0 +1,20 @@
+package pl.gorniak.bookweblab2;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutProgramActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_layout);
+        assert getSupportActionBar() != null;   //null check
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+}
---
+++ app/src/main/java/pl/gorniak/bookweblab2/MainActivity.java
@@ -0,0 +1,38 @@
+package pl.gorniak.bookweblab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.info:
+                Intent infopage = new Intent(MainActivity.this,AboutProgramActivity.class);
+                startActivity(infopage);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
